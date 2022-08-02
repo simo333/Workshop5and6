9608ebb238ef60af165b86cf75ad5667898adbf5
simo333 <szymon.333@wp.pl>
2022-08-03 00:20:23 +0200

    Row mapper for Book class

---
+++ src/main/java/pl/workshop5/model/BookRowMapper.java
@@ -0,0 +1,21 @@
+package pl.workshop5.model;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class BookRowMapper implements RowMapper<Book> {
+    @Override
+    public Book mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Book book = new Book();
+        book.setId(rs.getLong("id"));
+        book.setIsbn(rs.getString("isbn"));
+        book.setTitle(rs.getString("title"));
+        book.setAuthor(rs.getString("author"));
+        book.setPublisher(rs.getString("publisher"));
+        book.setType(rs.getString("type"));
+
+        return book;
+    }
+}

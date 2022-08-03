0f9d0593b8566b424cdd6b7260b5a814dcbcec3d
simo333 <szymon.333@wp.pl>
2022-08-03 21:45:25 +0200

    Added BookController - v1

--- src/main/java/pl/workshop5/controller/BookController.java
+++ src/main/java/pl/workshop5/controller/BookController.java
@@ -1,16 +1,44 @@
 package pl.workshop5.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.workshop5.model.Book;
+import pl.workshop5.model.BookDao;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/books")
 public class BookController {
 
-    @RequestMapping("/helloBook")
-    public Book helloBook() {
-        return new Book(1L, "9788324631766", "Thinking in Java",
-                "Bruce Eckel", "Helion", "programming");
+    private final BookDao bookDao;
+
+    public BookController(BookDao bookDao) {
+        this.bookDao = bookDao;
     }
+
+    @GetMapping
+    public List<Book> findAllBooks() {
+        return bookDao.all();
+    }
+
+    @GetMapping("/{id}")
+    public Book findById(@PathVariable Long id) {
+        return bookDao.one(id);
+    }
+
+    @PostMapping
+    public void createBook(@RequestBody Book book) {
+        bookDao.add(book);
+    }
+
+    @PutMapping("/{id}")
+    public void updateBook(@PathVariable Long id, @RequestBody Book book) {
+        bookDao.update(id, book);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable Long id) {
+        bookDao.delete(id);
+    }
+
 }

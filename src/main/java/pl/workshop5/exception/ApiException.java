721cb3fdf909fff94c06c849b899bd4061b2a532
simo333 <szymon.333@wp.pl>
2022-08-06 14:53:17 +0200

    Changed LocalDateTime to Instant

--- src/main/java/pl/workshop5/exception/ApiException.java
+++ src/main/java/pl/workshop5/exception/ApiException.java
@@ -4,20 +4,20 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.springframework.http.HttpStatus;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 
 @JsonIgnoreProperties({"stackTrace", "cause", "localizedMessage", "suppressed"})
 public class ApiException extends RuntimeException {
     private final HttpStatus httpStatus;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss[.SSS][XXX]")
-    private final LocalDateTime timeStamp = LocalDateTime.now();
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss[.SSS][XXX]", timezone = "UTC")
+    private final Instant timeStamp = Instant.now();
 
     public ApiException(String message, HttpStatus httpStatus) {
         super(message);
         this.httpStatus = httpStatus;
     }
 
-    public LocalDateTime getTimeStamp() {
+    public Instant getTimeStamp() {
         return timeStamp;
     }
 
